7cdf297409f1492b725f31adeb0c57bcea52e391
stager108 <yakusheva55@mail.ru>
2021-04-14 21:11:36 +0300

    fix points

--- experiment/scripts/generate_mut.c
+++ experiment/scripts/generate_mut.c
@@ -8,9 +8,10 @@
 const int MAX_SMALL_MUT_SIZE = 50;
 const int MIN_COPY_SIZE = 5000;
 const int COPY_INTERVAL = 5000;
+const int INTERVAL = 10000;
 
 const double INDEL_PROBABILITY = 0.0001;
-const double COPYNUMBER_PROBABILITY = 0.0001;
+const double COPYNUMBER_PROBABILITY = 0.001;
 const double POINT_MUTATION_PROBABILITY = 0.005;
 
 
@@ -18,6 +19,8 @@ char header[1000];
 FILE *infile, *config, *inner_config;
 char a;
 int filesize = 0;
+int position = 0;
+char *chromosome;
 
 void open_files(char *argv[]) {
     infile = fopen(argv[1], "r");
@@ -87,53 +90,33 @@ char random_point_mut(char inp, double prob) {
     }
 }
 
-int add_point_mutations(char *argv[]) {
-    open_files(argv);
-    fscanf(infile, "%[^\n]", header);
-    char *chromosome = get_chromosome(header);
-    printf("%s\n", chromosome);
-    int i = 0;
-    int filesize = 0;
-    while (fscanf(infile, "%c", &a) == 1) {
+int add_point_mutations(int edge) {
+    while ((fscanf(infile, "%c", &a) == 1) && (position < edge)) {
         int x = rand() % 3;
         char b = random_point_mut(a, POINT_MUTATION_PROBABILITY);
         double p = randfrom(0.0, 1.0);
         if ((is_nucleotide(a)) & (p < POINT_MUTATION_PROBABILITY)) {
             // 0 - del, 1 - replace, 2 - insert
             if (x == 0) {
-                fprintf(config, "%s %d %d 1.00 DEL \n", chromosome, i + 1, i + 2);
-                fprintf(inner_config, "d %d %d \n", i + 1, i + 2);
+                fprintf(config, "%s %d %d 1.00 DEL \n", chromosome, position + 1, position + 2);
+                fprintf(inner_config, "d %d %d\n", position, 1);
             }
-            if (x == 1) {
-                fprintf(config, "%s %d %d 1.00 DEL \n", chromosome, i + 1, i + 2);
-                fprintf(inner_config, "d %d %d \n", i + 1, i + 2);
-
-                fprintf(config, "%s %d %d 1.00 INS %c\n", chromosome, i + 1, i + 2, b);
-                fprintf(inner_config, "i %d %d %c\n", i + 1, i + 2, b);
-                filesize++;
-            }
-            if (x > 1) {
+            if (x > 0) {
                 filesize += 2;
-                fprintf(config, "%s %d %d 1.00 INS %c\n", chromosome, i + 1, i + 2, b);
-                fprintf(inner_config, "i %d %d %c\n", i + 1, i + 2, b);
+                fprintf(config, "%s %d %d 1.00 INS %c\n", chromosome, position + 1, position + 2, b);
+                fprintf(inner_config, "i %d %d %c\n", position, 1, b);
             }
-            i++;
+            position++;
         } else {
-            i += is_nucleotide(a);
+            position += is_nucleotide(a);
         }
     }
-    close_files();
     return 0;
 }
 
-int add_insertions(char *argv[]) {
+int add_insertions(int edge) {
 
-    open_files(argv);
-    fscanf(infile, "%[^\n]", header);
-    char *chromosome = get_chromosome(header);
-
-    int position = 0;
-    while (fscanf(infile, "%c", &a) == 1) {
+    while ((fscanf(infile, "%c", &a) == 1) && (position < edge)) {
         double p = randfrom(0.0, 1.0);
         if (p < INDEL_PROBABILITY) {
             int length = (rand() % MAX_SMALL_MUT_SIZE) + 1;
@@ -152,26 +135,20 @@ int add_insertions(char *argv[]) {
         filesize += is_nucleotide(a);
         position += is_nucleotide(a);
     }
-    close_files();
-    free(chromosome);
     return 0;
 }
 
 
-int add_deletions(char *argv[]) {
-
-    open_files(argv);
-    fscanf(infile, "%[^\n]", header);
-    char *chromosome = get_chromosome(header);
+int add_deletions(int edge) {
     int i = 0;
     int k = 0;
-    while (fscanf(infile, "%c", &a) == 1) {
+    while ((fscanf(infile, "%c", &a) == 1) && (position < edge)) {
         double p = randfrom(0.0, 1.0);
         if (p < INDEL_PROBABILITY) {
             int length = (rand() % MAX_SMALL_MUT_SIZE) + 1;
             k = 0;
             while (k < length) {
-                if (fscanf(infile, "%c", &a) != 1) {
+                if ((fscanf(infile, "%c", &a) != 1) || (position >= edge)) {
                     length = k;
                     break;
                 } else {
@@ -187,26 +164,18 @@ int add_deletions(char *argv[]) {
             i += is_nucleotide(a);
         }
     }
-    close_files();
-    free(chromosome);
     return 0;
 }
 
-int add_copies(char *argv[]) {
-
-    open_files(argv);
-    fscanf(infile, "%[^\n]", header);
-    char *chromosome = get_chromosome(header);
+int add_copies(int edge) {
     int k;
-
-    int position = 0;
-    while (fscanf(infile, "%c", &a) == 1) {
+    while ((fscanf(infile, "%c", &a) == 1) && (position < edge)) {
         double p = randfrom(0.0, 1.0);
         if (p < COPYNUMBER_PROBABILITY) {
             int length = (rand() % COPY_INTERVAL) + MIN_COPY_SIZE;
             k = 0;
             while (k < length) {
-                if (fscanf(infile, "%c", &a) != 1) {
+                if ((fscanf(infile, "%c", &a) != 1) || (position >= edge)) {
                     length = k;
                     break;
                 } else {
@@ -215,27 +184,20 @@ int add_copies(char *argv[]) {
                 }
             }
 
-            fprintf(config, "%s %d %d BIGDUP %d 1.0 ", chromosome, filesize + 1, filesize + length + 1, rand() % 3 + 1);
+            fprintf(config, "%s %d %d DUP %d 1.0 ", chromosome, filesize + 1, filesize + length + 1, rand() % 3 + 1);
             fprintf(inner_config, "c %d %d\n", filesize, length);
             filesize += length;
+            position += length;
             fprintf(config, "%c", '\n');
         }
         filesize += is_nucleotide(a);
         position += is_nucleotide(a);
     }
-
-    close_files();
-    free(chromosome);
     return 0;
 }
 
 int add_big_deletions(char *argv[]) {
-
-    open_files(argv);
-    fscanf(infile, "%[^\n]", header);
-    char *chromosome = get_chromosome(header);
     int k;
-
     int position = 0;
     while (fscanf(infile, "%c", &a) == 1) {
         double p = randfrom(0.0, 1.0);
@@ -252,11 +214,41 @@ int add_big_deletions(char *argv[]) {
                 }
             }
             fprintf(config, "%s %d %d BIGDEL 0.5 ", chromosome, filesize + 1, filesize + length + 1);
-            fprintf(inner_config, "d %d %d\n", filesize, length);
+            fprintf(inner_config, "d %d %d ", filesize, length);
             fprintf(config, "%c", '\n');
+            fprintf(inner_config, "%c", '\n');
         }
         filesize += is_nucleotide(a);
         position += is_nucleotide(a);
+    }
+    return 0;
+}
+
+int generate_mix(char *argv[], char opt) {
+
+    open_files(argv);
+    int length = 0;
+    fscanf(infile, "%[^\n]", header);
+    chromosome = get_chromosome(header);
+    while (fscanf(infile, "%c", &a) == 1) {
+        filesize += is_nucleotide(a);
+        position += is_nucleotide(a);
+        length = rand() % INTERVAL;
+        add_copies(position + length);
+        length = rand() % INTERVAL;
+        switch (opt) {
+            case 'd':
+                add_deletions(position + length);
+                break;
+            case 'i':
+                add_insertions(position + length);
+                break;
+            case 'p':
+                add_point_mutations(position + length);
+                break;
+            default:
+                break;
+        }
     }
 
     close_files();
@@ -268,23 +260,17 @@ int add_big_deletions(char *argv[]) {
 int main(int argc, char *argv[]) {
 
     int opt;
-    while ((opt = getopt(argc, argv, "dipcb")) != -1) {
+    while ((opt = getopt(argc, argv, "idp")) != -1) {
         switch (opt) {
-            case 'p':
-                add_point_mutations(argv);
-                break;
-            case 'd':
-                add_deletions(argv);
-                break;
             case 'i':
-                add_insertions(argv);
+            case 'd':
+            case 'p':
+                generate_mix(argv, opt);
                 break;
-            case 'c':
-                add_copies(argv);
-                break;
+
             default:
                 fprintf(stderr, "Usage: %s [-pr] [input file, output config file]\n", argv[0]);
-                fprintf(stderr, "-p point mutations -i insertions -d deletions -c copies -b big deletions\n");
+                fprintf(stderr, "-i insertions -d deletions -p point mutations\n");
                 exit(EXIT_FAILURE);
         }
     }
