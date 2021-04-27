83bc776647b8cb0887dd76c2efbb55d26f45e261
sophia_yakusheva <sofia.yakusheva@bostongene.com>
2021-04-27 12:01:30 +0300

    fix sequenza-utils running

--- experiment/scripts/generate_seq_tests.sh
+++ experiment/scripts/generate_seq_tests.sh
@@ -17,9 +17,11 @@ do
   TESTDIR=$1
   ./scripts/apply_mut.exe ${GENOME} ${TESTDIR}/genome_mut_${MODE}${i}.fa $TESTDIR/${TESTPREFIX}0${i}.txt -a
   ./scripts/run_iss_bwa.sh ${TESTDIR} ${TESTDIR}/genome_mut_${MODE}${i}.fa mutated_${MODE}${i} 50 ${GENOME}
-  sequenza-utils gc_wiggle -f ${TESTDIR}/genome_mut_${MODE}${i}.fa -o ${TESTDIR}/genome_mut_${MODE}${i}.wig -w 1000
+
+  sequenza-utils gc_wiggle -f ${TESTDIR}/genome_mut_${MODE}${i}.fa -o ${TESTDIR}/genome_mut_${MODE}${i}.wig -w 50
   sequenza-utils bam2seqz -n ${TESTDIR}/aligned.bam -t ${TESTDIR}/mutated_${MODE}${i}.bam -gc ${TESTDIR}/genome_mut_${MODE}${i}.wig -F ${GENOME} -o ${TESTDIR}/genome_mut_${MODE}${i}_seq.txt
-  
+  sequenza-utils seqz_binning --seqz ${TESTDIR}/genome_mut_${MODE}${i}_seq.txt -w 50 -o ${TESTDIR}/genome_mut_${MODE}${i}_seq.seqz
+
   ./scripts/run_strelka.sh ${TESTDIR} aligned mutated_${MODE}${i} ${TESTPREFIX}0${i} ${GENOME}
 
 done
